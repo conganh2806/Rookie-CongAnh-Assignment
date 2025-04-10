28b46b8ff59b48151a4775d2f9125ceef1d98f75
Nguyen Cong Anh <ncanh2806@gmail.com>
2025-04-10 10:48:48 +0700

    - fix unit of work

--- ECommerce.Application/Services/Product/ProductService.cs
+++ ECommerce.Application/Services/Product/ProductService.cs
@@ -9,31 +9,29 @@ namespace ECommerce.Application.Services.Product;
 public class ProductService : IProductService
 {
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IProductRepository _productRepository;
     private readonly IMapper _mapper;
 
-    public ProductService(IUnitOfWork unitOfWork, IProductRepository productRepository, IMapper mapper)
+    public ProductService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
-        _productRepository = productRepository;
         _mapper = mapper;
     }
 
     public async Task<List<ProductDto>> GetFeaturedProductsAsync()
     {
-        var products = await _productRepository.GetFeaturedProductsAsync();
+        var products = await _unitOfWork.ProductRepository.GetFeaturedProductsAsync();
         return _mapper.Map<List<ProductDto>>(products);
     }
 
     public async Task<List<ProductDto>> GetByCategoryAsync(Guid categoryId)
     {
-        var products = await _productRepository.GetByCategoryAsync(categoryId);
+        var products = await _unitOfWork.ProductRepository.GetByCategoryAsync(categoryId);
         return _mapper.Map<List<ProductDto>>(products);
     }
 
     public async Task<ProductDto?> GetByIdAsync(Guid id)
     {
-        var product = await _productRepository.GetByIdAsync(id);
+        var product = await _unitOfWork.ProductRepository.GetByIdAsync(id);
         return product != null ? _mapper.Map<ProductDto>(product) : null;
     }
 }
--- ECommerce.Domain/Interfaces/IUnitOfWork.cs
+++ ECommerce.Domain/Interfaces/IUnitOfWork.cs
@@ -1,7 +1,10 @@
+using ECommerce.Application.Domain.Interfaces;
+
 namespace ECommerce.Domain.Interfaces 
 { 
     public interface IUnitOfWork : IDisposable
     {
+        IProductRepository ProductRepository { get; }
         Task<int> SaveChangesAsync();
     }
 }
