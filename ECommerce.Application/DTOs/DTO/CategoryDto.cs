2caeb6ecc906ab26bba4cf406bfbdd001e2ed534
Nguyen Cong Anh <ncanh2806@gmail.com>
2025-04-13 17:35:36 +0700

    - add category DTO

---
+++ ECommerce.Application/DTOs/DTO/CategoryDto.cs
@@ -0,0 +1,8 @@
+namespace ECommerce.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public string Name { get; set; } = default!;
+        public string Slug { get; set; } = default!;
+    }
+}
