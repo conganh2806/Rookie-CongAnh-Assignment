af54c1751ff58b1ffaffd6b5f559edddeb1fe7e2
Nguyen Cong Anh <ncanh2806@gmail.com>
2025-04-16 13:41:36 +0700

    - add global exception

---
+++ ECommerce.API/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,80 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+using ECommerce.Application.Common.Utilities.Exceptions;
+using ECommerce.Common.Utilities;
+using ECommerce.JsonNamingPolicy;
+
+namespace ECommerce.API.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+
+            var response = context.Response;
+
+            if (response.HasStarted)
+                return;
+            response.ContentType = "application/json";
+
+            var errorDetail = new ErrorDetails()
+            {
+                StatusCode = (int)System.Net.HttpStatusCode.InternalServerError,
+                Message = exception.Message
+            };
+            switch (exception)
+            {
+                case NotFoundException e:
+                    // Custom not found
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    break;
+                case UnAuthorizedException e:
+                    // Custom not found
+                    response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    break;
+                case ForbiddenException e:
+                    response.StatusCode = (int)HttpStatusCode.Forbidden;
+                    break;
+                case BadRequestException e:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    break;
+                case ValidationException e:
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    errorDetail.Message = "Object validation error.";
+                    break;
+                default:
+                    // unhandled error
+                    response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    errorDetail.Message = exception.Message;
+                    break;
+            }
+
+            errorDetail.StatusCode = response.StatusCode;
+            var serializeOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = SnakeCaseNamingPolicy.Instance,
+                WriteIndented = true
+            };
+            await response.WriteAsync(JsonSerializer.Serialize(errorDetail, serializeOptions));
+        }
+    }
+}
