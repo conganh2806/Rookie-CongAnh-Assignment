76bdb06069f1d4f08d927d6cf2f0a0437dfdf434
Nguyen Cong Anh <ncanh2806@gmail.com>
2025-04-16 13:52:16 +0700

    - add: dependency injection service

--- ECommerce.Application/DependencyInjection.cs
+++ ECommerce.Application/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using ECommerce.Application.Interfaces;
 using ECommerce.Application.Mapping;
+using ECommerce.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ECommerce.Application;
@@ -8,7 +9,10 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddScoped<IUserService, UserService>();
         services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IOrderService, OrderService>();
         services.AddAutoMapper(typeof(AutoMappingProfile));
         return services;
     }
