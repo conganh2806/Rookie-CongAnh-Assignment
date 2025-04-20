2ebf0b6ceeabe502a132461bb32e4b57906713f3
Nguyen Cong Anh <ncanh2806@gmail.com>
2025-04-20 16:30:00 +0700

    - remove: comment

--- ECommerce.Application/DTOs/Common/CreatePresignedUrlRequest.cs
+++ ECommerce.Application/DTOs/Common/CreatePresignedUrlRequest.cs
@@ -4,7 +4,7 @@ namespace ECommerce.Application.DTOs.Common
     {
         public string FileName { get; set; } = default!;
         public string ContentType { get; set; } = default!;
-        public string ObjectType { get; set; } = default!; // e.g. "product"
-        public string ObjectId { get; set; } = default!;   // e.g. product Id
+        public string ObjectType { get; set; } = default!;
+        public string ObjectId { get; set; } = default!;
     }
 }
